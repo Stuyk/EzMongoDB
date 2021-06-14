08e77abe5b7c26256e6624bad9b8b61162915abc
Stuyk <stuykgaming@gmail.com>
2021-06-14 16:24:14 -0600

    Add Missing Types

---
+++ dist/utility/logger.d.ts
@@ -0,0 +1,4 @@
+export declare class Logger {
+    static log(value: string): void;
+    static error(value: string): void;
+}
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     testEnvironment: 'node',
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+    
 };
